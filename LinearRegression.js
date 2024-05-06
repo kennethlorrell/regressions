0dd2329a2a4b35dfe3e2ff65324c9427a3232a9f
deepdark <39443507+kennethlorrell@users.noreply.github.com>
2024-05-07 00:28:26 +0300

    Extract duplicate functionality to processFeatures method

--- LinearRegression.js
+++ LinearRegression.js
@@ -2,12 +2,8 @@ const tf = require('@tensorflow/tfjs');
 
 class LinearRegression {
   constructor(features, labels, options) {
-    this.features = tf.tensor(features);
+    this.features = this.processFeatures(features);
     this.labels = tf.tensor(labels);
-
-    this.features = tf
-      .ones([this.features.shape[0], 1])
-      .concat(this.features, 1);
 
     this.weights = tf.zeros([2, 1]);
 
@@ -38,10 +34,8 @@ class LinearRegression {
   }
 
   test(testFeatures, testLabels) {
-    testFeatures = tf.tensor(testFeatures);
+    testFeatures = this.processFeatures(testFeatures);
     testLabels = tf.tensor(testLabels);
-
-    testFeatures = tf.ones([testFeatures.shape[0], 1]).concat(testFeatures, 1);
 
     const predictions = testFeatures.matMul(this.weights);
 
@@ -58,6 +52,13 @@ class LinearRegression {
       .get();
 
     return 1 - residuals / total;
+  }
+
+  processFeatures(features) {
+    features = tf.tensor(features);
+    features = tf.ones([features.shape[0], 1]).concat(features, 1);
+
+    return features;
   }
 }
 
