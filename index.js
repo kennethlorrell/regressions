e3248fd263db5154dbf0d17e1e147b2c669527a7
deepdark <39443507+kennethlorrell@users.noreply.github.com>
2024-05-07 23:38:22 +0300

    Add the ability to predict expected results

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 
 const regression = new LinearRegression(features, labels, {
   learningRate: 0.1,
-  iterations: 5,
+  iterations: 3,
   batchSize: 10
 });
 
@@ -32,5 +32,9 @@ plot({
 });
 
 console.log(regression.test(testFeatures, testLabels));
+
+regression.predict([
+  [120, 2, 380]
+]).print();
 
 // console.log(`Updated M is: ${regression.weights.get(1, 0)}, updated B is: ${regression.weights.get(0, 0)}`);
