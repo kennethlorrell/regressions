8f0dd2287b0d09d47e39808689c435062bf0655b
deepdark <39443507+kennethlorrell@users.noreply.github.com>
2024-05-07 22:31:17 +0300

    Add plotting of image with MSE graph

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 const LinearRegression = require('./LinearRegression');
+const plot = require('node-remote-plot');
 
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
   shuffle: true,
@@ -17,11 +18,18 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 });
 
 const regression = new LinearRegression(features, labels, {
-  learningRate: 0.01,
+  learningRate: 0.1,
   iterations: 100
 });
 
 regression.train();
+
+plot({
+  x: regression.mseHistory.reverse(),
+  xLabel: 'Iteration #',
+  yLabel: 'Mean Squared Error'
+});
+
 console.log(regression.test(testFeatures, testLabels));
 
 // console.log(`Updated M is: ${regression.weights.get(1, 0)}, updated B is: ${regression.weights.get(0, 0)}`);
